9f423a6981845071c1cc2f94e5dc4ac368e00e25
mahmud444t <ferdausemahmud@gmail.com>
2025-10-20 12:30:56 +0600

    added favicon and fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,6 @@ const roboto = Roboto({
   subsets: ["latin"],
 });
 
-
 export const metadata = {
   title: {
     default: "Alharf - Women's Fashion, Footwear & Accessories",
@@ -29,7 +28,6 @@ export const metadata = {
     "Alharf offers premium women's fashion, including dresses, tops, shoes, handbags, accessories, and more. Elevate your look with our modern and elegant collections.",
 };
 
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -37,7 +35,7 @@ export default function RootLayout({ children }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        {/* Data Layer Initialization */}
+        {/* ✅ Google Tag Manager: Data Layer */}
         <Script
           id="gtm-data-layer"
           strategy="beforeInteractive"
@@ -46,68 +44,81 @@ export default function RootLayout({ children }) {
           }}
         />
 
-        {/* Google Tag Manager */}
-        {/* <Script
+        {/* ✅ Google Tag Manager: Main Script (head equivalent) */}
+        <Script
           id="google-tag-manager"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-T7N6H46R');
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KJZHWX43');
             `,
-          }}
-        /> */}
-
-        {/* new server side tracking script */}
-
-        <Script
-          id="google-tag-manager-custom"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-      (function(w,d,s,l,i){
-        w[l] = w[l] || [];
-        w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-        var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s);
-        j.async = true;
-        j.src = "https://load.sst.alharfbd.com/9lbcdttzm.js?" + i;
-        f.parentNode.insertBefore(j, f);
-      })(window, document, 'script', 'dataLayer', '7=aWQ9R1RNLVQ3TjZINDZS&page=2');
-    `,
           }}
         />
 
-
+        {/* ✅ Meta Pixel (Facebook Pixel) */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s){
+                if(f.fbq)return;
+                n=f.fbq=function(){
+                  n.callMethod ?
+                  n.callMethod.apply(n,arguments) : n.queue.push(arguments)
+                };
+                if(!f._fbq)f._fbq=n;
+                n.push=n;
+                n.loaded=!0;
+                n.version='2.0';
+                n.queue=[];
+                t=b.createElement(e);
+                t.async=!0;
+                t.src=v;
+                s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)
+              }(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1141675167899351');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </head>
 
       <body
         className={`${assistant.className} bg-[#f3f3f3]`}
         suppressHydrationWarning={true}
       >
-        {/* Google Tag Manager (noscript) - Placed at the very top */}
-        {/*  <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-T7N6H46R"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}  // ✅ Correct
-          ></iframe>
-        </noscript> */}
-
-        {/* SST Server Side Tracking (noscript) */}
+        {/* ✅ Google Tag Manager (noscript fallback) */}
         <noscript>
           <iframe
-            src="https://load.sst.alharfbd.com/ns.html?id=GTM-T7N6H46R"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KJZHWX43"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
 
+        {/* ✅ Meta Pixel (noscript fallback) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1141675167899351&ev=PageView&noscript=1"
+          />
+        </noscript>
+
+        {/* ✅ App Layout */}
         <AuthProvider>
           <CartProvider>
             <Navbar />
